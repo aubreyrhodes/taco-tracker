2fc144a4375f280ff8a6300ef3763827b6992fc0
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2012-06-28 22:19:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TacoTracker::Application.config.secret_token = 'cb92d21f60c556188b1c627e64052d88368432144345367dc8d17bab3891c9598923f9294587e74bb92de5428c84634251e0a25c6f979ce56079cd5ac2595a91'
